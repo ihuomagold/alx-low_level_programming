931369db81641568d10d2e3bb1874d834d9d4259
Ihuoma Ogbuji <ihuomagold53@gmail.com>
2022-07-05 14:53:34 -0700

    added tasks 2 - 11

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _abs - computes the absolute value of an integer.
+ * @r - the integer to be computed
+ * Return: the absolute value of int
+ */
+
+int _abs(int r)
+{
+	if (r >= 0)
+	{
+		return (r);
+	}
+	return (-r);
+}
