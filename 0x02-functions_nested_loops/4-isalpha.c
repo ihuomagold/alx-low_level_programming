5f411727834ec4da6eb540a795c70eb778086c23
Ihuoma Ogbuji <ihuomagold53@gmail.com>
2022-07-05 14:59:08 -0700

    corrected task 4

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -2,7 +2,7 @@
 
 /**
  * _isalpha - checks for alphabetic character.
- * @c - character to be checked
+ * @c: character to be checked
  * Returns: 1 if c is a letter, lowercase or uppercase,
  * 0 if otherwise
  */
