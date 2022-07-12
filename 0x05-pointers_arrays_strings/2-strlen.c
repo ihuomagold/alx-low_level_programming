ad8e229c5eb4d4b78f41b6962fd069133f5a06ae
Ihuoma Ogbuji <ihuomagold53@gmail.com>
2022-07-12 14:02:17 -0700

    added 1-3

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the length of a string
+ * @s: pointer to the string
+ * Return: the length of the string
+ */
+
+int _strlen(char *s)
+{
+	int c;
+
+	c = 0;
+
+	while (s[c] != '\0')
+	{
+		c++;
+	}
+
+	return (c);
+}
